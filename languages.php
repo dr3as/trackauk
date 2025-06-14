acd58283a2e081bacdf3c1c09bb911ca6a88e04e
Andreas <43793692+dr3as@users.noreply.github.com>
2025-06-14 18:00:32 +0200

    Update languages.php

--- languages.php
+++ languages.php
@@ -1,6 +1,10 @@
 <?php
 //English
-
+$language_id1 = 1;
+$language_name1 = "English";
+$language_text_yes1 = "yes";
 //Norwegian
-
+$language_id2 = 2;
+$language_name2 = "Norsk";
+$language_text_yes2 = "ja";
 ?>
