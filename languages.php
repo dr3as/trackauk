78c565eeb58508c6d8fca02fac6c1030c5a44f69
Andreas <43793692+dr3as@users.noreply.github.com>
2025-06-14 19:59:17 +0200

    .

--- languages.php
+++ languages.php
@@ -3,8 +3,10 @@
 $language_id1 = 1;
 $language_name1 = "English";
 $language_text_yes1 = "yes";
+$language_text_yes1 = "no";
 //Norwegian
 $language_id2 = 2;
 $language_name2 = "Norsk";
 $language_text_yes2 = "ja";
+$language_text_no2 = "nei";
 ?>
