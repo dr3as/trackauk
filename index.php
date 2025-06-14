17b7d4536600397cc3c789dd9f27cf44af0d3b52
Andreas <43793692+dr3as@users.noreply.github.com>
2025-06-14 22:37:02 +0200

    .

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
-//errorcode or not
-//error_reporting(E_ALL);
+errorcode or not
+error_reporting(E_ALL);
 //ini_set('display_errors', '1');
-error_reporting(0);
-ini_set('display_errors', '0');
+//error_reporting(0);
+//ini_set('display_errors', '0');
 session_start();
 //add configfiles
 include "config.php";
