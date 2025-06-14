e724b364ca52debe307e69ec3b75668dd94d2b3f
Andreas <43793692+dr3as@users.noreply.github.com>
2025-06-14 18:18:01 +0200

    .

--- planters.php
+++ planters.php
@@ -2,22 +2,24 @@
 //Auk
 $planter_1_id = 1;
 $planter_1_name = "Auk";
-$planter_1_icon = "path/to/img";
+$planter_1_icon = "img/Auk.webp";
+$planter_1_plant_slots = 6;
 
 //Auk mini
-$planter_1_id = 2;
-$planter_1_name = "Auk mini";
-$planter_1_icon = "path/to/img2";
+$planter_2_id = 2;
+$planter_2_name = "Auk mini";
+$planter_2_icon = "img/Aukmini.webp";
+$planter_2_plant_slots = 4;
 
 //Generic 1
-$planter_1_id = 3;
-$planter_1_name = "Generic 1 Plant";
-$planter_1_icon = "path/to/img3";
+$planter_3_id = 3;
+$planter_3_name = "Generic 1 Plant";
+$planter_3_icon = "path/to/img3";
 
 //Generic 2
-$planter_1_id = 4;
-$planter_1_name = "Generic 2 Plant";
-$planter_1_icon = "path/to/img4";
+$planter_4_id = 4;
+$planter_4_name = "Generic 2 Plant";
+$planter_4_icon = "path/to/img4";
 
 
 ?>
