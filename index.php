612f951d696c00f807f4a9bdd93ab4bdee29916c
Andreas <43793692+dr3as@users.noreply.github.com>
2025-06-14 22:37:19 +0200

    .

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
-errorcode or not
+//errorcode or not
 error_reporting(E_ALL);
-//ini_set('display_errors', '1');
+ini_set('display_errors', '1');
 //error_reporting(0);
 //ini_set('display_errors', '0');
 session_start();
