80760e6888c342ce1d938ee049ba8e12f7ef3904
Andreas <43793692+dr3as@users.noreply.github.com>
2025-06-14 09:17:36 +0200

    .

--- index.php
+++ index.php
@@ -10,9 +10,9 @@
         <br>
         <a href="index.php?show=timeline">Timeline</a>&nbsp &nbsp <a href="index.php?show=stats">Stats</a>&nbsp &nbsp <a href="index.phpshow=add">Add</a>&nbsp &nbsp <a href="index.php?show=settings">Settings</a>
         <?php
-        $show = "default";
+        $show == "default";
         if $_GET['show']:
-        $show = $_GET['show'];
+        $show == $_GET['show'];
         endif;
         if $show = "default":
             echo "Default";
