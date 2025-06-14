7bb233d0242737f73f9a9ca82a9f3917ac646821
Andreas <43793692+dr3as@users.noreply.github.com>
2025-06-14 17:49:59 +0200

    Create planters.php

---
+++ planters.php
@@ -0,0 +1,23 @@
+<?php
+//Auk
+$planter_1_id = 1;
+$planter_1_name = "Auk";
+$planter_1_icon = "path/to/img";
+
+//Auk mini
+$planter_1_id = 2;
+$planter_1_name = "Auk mini";
+$planter_1_icon = "path/to/img2";
+
+//Generic 1
+$planter_1_id = 3;
+$planter_1_name = "Generic 1 Plant";
+$planter_1_icon = "path/to/img3";
+
+//Generic 2
+$planter_1_id = 4;
+$planter_1_name = "Generic 2 Plant";
+$planter_1_icon = "path/to/img4";
+
+
+?>
