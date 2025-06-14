daf2371011ee58298afa99064f8533c9d77fbc94
Andreas <43793692+dr3as@users.noreply.github.com>
2025-06-14 08:16:09 +0200

    .

--- index.php
+++ index.php
@@ -3,8 +3,8 @@
         <title>AukTracker</title>
     </head>
     <body>
-        <h1>AukTacker</h1>
+        <h1>AukTracker</h1>Track your plants...
         <br>
-        <a href="index.php?blabla">Timeline</a>&nbsp 
+        <a href="index.php?blabla">Timeline</a>&nbsp &nbsp <a href="index.php?blabla">Stats</a>&nbsp &nbsp <a href="index.php?blabla">Add</a>&nbsp &nbsp <a href="index.php?blabla">Settings</a>
     </body>
 </html>
