6efc27c70c284d025c3983a46890b25fc47cc995
Andreas <43793692+dr3as@users.noreply.github.com>
2025-06-14 09:31:03 +0200

    .

--- index.php
+++ index.php
@@ -12,11 +12,13 @@ ini_set('display_errors', '1');
         <br>
         <br>
         <br>
-        <a href="index.php?show=timeline">Timeline</a>&nbsp &nbsp <a href="index.php?show=stats">Stats</a>&nbsp &nbsp <a href="index.phpshow=add">Add</a>&nbsp &nbsp <a href="index.php?show=settings">Settings</a>
+        <a href="index.php?show=timeline">Timeline</a>&nbsp &nbsp <a href="index.php?show=stats">Stats</a>&nbsp &nbsp <a href="index.php?show=add">Add</a>&nbsp &nbsp <a href="index.php?show=settings">Settings</a>
         <?php
-        $show == "default";
-        if ($_GET["show"])
-        $show == $_GET["show"];
+        if (is_null($_GET["show"]))
+            $show == "default";
+        else {
+            $show == $_GET["show"];
+        }
         if ($show = "default")
             echo "Default";
         ?>
