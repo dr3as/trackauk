d1d8efd0704495986a06df856bae31c133d90946
Andreas <43793692+dr3as@users.noreply.github.com>
2025-06-14 09:31:45 +0200

    .

--- index.php
+++ index.php
@@ -17,6 +17,7 @@ ini_set('display_errors', '1');
         if (is_null($_GET["show"]))
             $show == "default";
         else {
+            echo $_GET["show"];
             $show == $_GET["show"];
         }
         if ($show = "default")
