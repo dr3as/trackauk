e2e92ad7b9ca4a3eba39de07a353ce145942fac1
Andreas <43793692+dr3as@users.noreply.github.com>
2025-06-14 09:16:13 +0200

    .

--- index.php
+++ index.php
@@ -8,6 +8,15 @@
         <br>
         <br>
         <br>
-        <a href="index.php?blabla">Timeline</a>&nbsp &nbsp <a href="index.php?blabla">Stats</a>&nbsp &nbsp <a href="index.php?blabla">Add</a>&nbsp &nbsp <a href="index.php?blabla">Settings</a>
+        <a href="index.php?show=timeline">Timeline</a>&nbsp &nbsp <a href="index.php?show=stats">Stats</a>&nbsp &nbsp <a href="index.phpshow=add">Add</a>&nbsp &nbsp <a href="index.php?show=settings">Settings</a>
+        <?php
+        $show = "default";
+        if $_GET['show']:
+        $show = $_GET['show'];
+        endif;
+        if $show = "default":
+            echo "Default";
+        endif;
+        ?>
     </body>
 </html>
