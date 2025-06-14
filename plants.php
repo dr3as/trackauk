15eed6db1d0afc596621387a3284746c3de8436c
Andreas <43793692+dr3as@users.noreply.github.com>
2025-06-14 23:08:09 +0200

    .

--- plants.php
+++ plants.php
@@ -4,4 +4,24 @@ $plant_1_id = 1;
 $plant_1_name = "Basil";
 $plant_1_icon = "path/to/img3";
 
+//Pak Choi
+$plant_1_id = 2;
+$plant_1_name = "Pak Choi";
+$plant_1_icon = "path/to/img3";
+
+//Parsley
+$plant_1_id = 3;
+$plant_1_name = "Parsley";
+$plant_1_icon = "path/to/img3";
+
+//Mint
+$plant_1_id = 4;
+$plant_1_name = "Mint";
+$plant_1_icon = "path/to/img3";
+
+//Carrot
+$plant_1_id = 5;
+$plant_1_name = "Carrot";
+$plant_1_icon = "path/to/img3";
+
 ?>
